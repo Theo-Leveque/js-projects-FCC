4bd96fe8f7ab48ed10370b708fe0c73aa2f29549
Daawa-Karma <theo.leveque@gmail.com>
2020-11-16 20:27:15 -0800

    finished project 12

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,53 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2021, 2, 16, 18, 54, 0);
+
+const year = futureDate.getFullYear();
+const month = months[futureDate.getMonth()];
+const day = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+
+giveaway.textContent = `giveaway ends on ${day}, ${date} ${month} ${year} at ${hours}:${mins}`;
+
+//future time in ms
+const futureTime = futureDate.getTime();
+
+const getRemainingTime = () => {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  //values in ms
+  const oneWeek = 7 * 24 * 60 * 60 * 1000;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 100;
+  const oneMinute = 60 * 1000;
+  // calculate all values
+  const weeks = Math.floor(t / oneWeek);
+  const days = Math.floor((t % oneWeek) / oneDay);
+  const hours = Math.floor((t % oneDay) / oneHour);
+  const minutes = Math.floor((t % oneHour) / oneMinute);
+  const seconds = Math.floor((t % oneMinute) / 1000);
+
+  //set values array
+  const values = [weeks, days, hours, minutes, seconds];
+
+  const format = (item) => (item < 10 ? `0${item}` : item);
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
+  }
+};
+//countdown
+let countdown = setInterval(getRemainingTime, 1000);
+getRemainingTime();
